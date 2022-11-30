4fc6f47b11ddd831c238fd0baa324e9a865db6eb
Yama <pyama.ndiaye@gmail.com>
2022-11-29 22:33:34 -0500

    siuuu

---
+++ src/BST.java
@@ -0,0 +1,284 @@
+public class BST {
+
+    public Node root;
+
+    /*
+
+     * Class containing left and right child of current node and the key data To
+
+     * link nodes to each other
+
+     */
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+
+    }
+
+    public BST() {
+        this.root = null;
+    }
+
+    /**
+     * Search the tree for data
+     *
+     * @param data to look for
+     * @return true if found, otherwise false
+     */
+
+    public boolean search(int data) {
+
+        Node currentNode = root;
+
+        while (currentNode != null) {
+
+            if (currentNode.data == data) {
+
+                return true;
+
+            } else if (currentNode.data > data) {
+
+                currentNode = currentNode.left;
+
+            } else {
+
+                currentNode = currentNode.right;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    /*
+
+     * To delete a node we have consider three cases: 1. root has no children that
+
+     * means its the root node 2. If node to be deleted is either leftmost node or
+
+     * rightmost 3. leftmost node is internal and has both left and right child.
+
+     */
+
+    public boolean remove(int value) {
+
+        BST.Node parent = root;
+
+        BST.Node current = root;
+
+        boolean isLeftChild = false;
+
+        while (current.data != value) {
+
+            parent = current;
+
+            if (current.data > value) {
+
+                isLeftChild = true;
+
+                current = current.left;
+
+            } else {
+
+                isLeftChild = false;
+
+                current = current.right;
+
+            }
+
+            if (current == null) {
+
+                return false;
+
+            }
+
+        }
+
+// Node which needs to be deleted will be present in current variable
+
+// Case 1: If node to be deleted doesn't have any child means it is a root node
+
+        if (current.left == null && current.right == null) {
+
+            if (current == root) {
+
+                root = null;
+
+            }
+
+            if (isLeftChild) {
+
+                parent.left = null;
+
+            } else {
+
+                parent.right = null;
+
+            }
+
+        }
+
+// Case 2: If node to be deleted have only one child
+
+        else if (current.right == null) {
+
+            if (current == root) {
+
+                root = current.left;
+
+            } else if (isLeftChild) {
+
+                parent.left = current.left;
+
+            } else {
+
+                parent.right = current.left;
+
+            }
+
+        } else if (current.left == null) {
+
+            if (current == root) {
+
+                root = current.right;
+
+            } else if (isLeftChild) {
+
+                parent.left = current.right;
+
+            } else {
+
+                parent.right = current.right;
+
+            }
+
+        }
+
+// Case 3: If node to be deleted is an internal node with both left and right
+
+// child
+
+        else if (current.left != null && current.right != null) {
+
+            BST.Node successor = getMinimum(current);
+
+            if (current == root) {
+
+                root = successor;
+
+            } else if (isLeftChild) {
+
+                parent.left = successor;
+
+            } else {
+
+                parent.right = successor;
+
+            }
+
+            successor.left = current.left;
+
+        }
+
+        return true;
+
+    }
+
+    /**
+     * find the minimum close to the node
+     *
+     * @param node to look for
+     * @return closet minimum node
+     */
+
+    public Node getMinimum(Node node) {
+
+        Node nextHigher = null;
+
+        Node nextHigherParent = null;
+
+        Node current = node.right;
+
+        while (current != null) {
+
+            nextHigherParent = nextHigher;
+
+            nextHigher = current;
+
+            current = current.left;
+
+        }
+
+        if (nextHigher != node.right) {
+
+            nextHigherParent.left = nextHigher.right;
+
+            nextHigher.right = node.right;
+
+        }
+
+        return nextHigher;
+
+    }
+
+    /**
+     * Add value to the tree.
+     *
+     * @param value to add to Tree
+     */
+
+    public void insert(int value) {
+        Node v = new Node(value);
+
+        if (root == null) {
+            root = v;
+            return;
+        }
+
+        Node current = root;
+
+        while (true) {
+            if (value < current.data) {
+                if (current.left == null) {
+                    current.left = v;
+                    return;
+                } else current = current.left;
+            } else {
+                if (current.right == null) {
+                    current.right = v;
+                    return;
+                } else current = current.right;
+            }
+        }
+    }
+
+    /**
+     * inorder traversal of tree
+     *
+     * @param root tells the starting nodeF
+     */
+
+    public void inorderTraversal(Node root) {
+
+        if (root != null) {
+
+            inorderTraversal(root.left);
+
+            System.out.print(root.data + "\t");
+
+            inorderTraversal(root.right);
+
+        }
+
+    }
+}
--- src/RunME.java
+++ src/RunME.java
@@ -1,5 +1,87 @@
+import java.util.Arrays;
+import java.io.*;
+
 public class RunME {
-    public static void main(String[] args) {
-        System.out.println("Hello There");
+    public static void main(String[] args) throws IOException {
+        BST tree1 = new BST();
+        tree1.insert(1);
+        tree1.insert(2);
+        tree1.insert(3);
+        tree1.insert(4);
+        tree1.insert(5);
+        tree1.insert(6);
+        tree1.insert(7);
+        tree1.insert(8);
+        tree1.insert(9);
+        tree1.insert(10);
+        tree1.insert(11);
+        tree1.insert(12);
+        tree1.insert(13);
+        tree1.insert(14);
+        tree1.insert(15);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println();
+
+        BufferedWriter BW1A = new BufferedWriter(new FileWriter("search(1)A.txt"));
+        long[] search1timesA = new long[100000];
+        for (int i = 0; i < 100000; i++) {
+            long start = System.nanoTime();
+            tree1.search(1);
+            search1timesA[i] = System.nanoTime() - start;
+        }
+        BW1A.write("CPU times for search(1) in tree1 100000times in nanoseconds.");
+        BW1A.newLine();
+        for (long i : search1timesA) {
+            BW1A.write(i + "ns");
+            BW1A.newLine();
+        }
+        BW1A.close();
+
+        BufferedWriter BW15A = new BufferedWriter(new FileWriter("search(15)A.txt"));
+        long[] search15timesA = new long[100000];
+        for (int i = 0; i < 100000; i++) {
+            long start = System.nanoTime();
+            tree1.search(15);
+            search15timesA[i] = System.nanoTime() - start;
+        }
+        BW15A.write("CPU times for search(15) in tree1 100000times in nanoseconds.");
+        BW15A.newLine();
+        for (long i : search15timesA) {
+            BW15A.write(i + "ns");
+            BW15A.newLine();
+        }
+        BW15A.close();
+
+        tree1.remove(5);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println();
+        tree1.remove(15);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println();
+        tree1.remove(1);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println();
+        tree1.insert(2);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println();
+
+        BST tree2 = new BST();
+        tree2.insert(8);
+        tree2.insert(4);
+        tree2.insert(12);
+        tree2.insert(2);
+        tree2.insert(6);
+        tree2.insert(10);
+        tree2.insert(14);
+        tree2.insert(1);
+        tree2.insert(3);
+        tree2.insert(5);
+        tree2.insert(7);
+        tree2.insert(9);
+        tree2.insert(11);
+        tree2.insert(13);
+        tree2.insert(15);
+        tree2.inorderTraversal(tree2.root);
+        System.out.println();
     }
 }
